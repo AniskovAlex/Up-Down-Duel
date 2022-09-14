a24086d67ac260e9ce978430e640d9e6c579136f
AniskovAlex <54923203+AniskovAlex@users.noreply.github.com>
2022-09-14 16:39:56 +0700

    Поле, стены и игрок
    -Добавлены стены
    -Добавлен объект игрока и логика перемещения

---
+++ Assets/_scripts/Modules/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    public float moveSpeed = 1f;
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotation = Input.GetAxis("Horizontal");
+        if (rotation != 0)
+        {
+            gameObject.transform.Rotate(-Vector3.forward, rotation * rotationSpeed);
+        }
+
+        float move = Input.GetAxis("Vertical");
+
+        rigidbody.velocity = gameObject.transform.up * move * moveSpeed;
+    }
+}
